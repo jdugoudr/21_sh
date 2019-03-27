f6539b399dc340167e19dbe1110da35807b9c152
jdugoudr@student.42.fr <jdugoudr@e2r9p20.42.fr>
2019-03-27 22:50:35 +0100

    ...

---
+++ srcs/parser/sort_redirect.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_redirect.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdugoudr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/27 22:17:03 by jdugoudr          #+#    #+#             */
+/*   Updated: 2019/03/27 22:21:03 by jdugoudr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "token_define.h"
+#include "parser.h"
+#include "libft.h"
+
+int	sort_redirect(t_ast **head)
+{
+	(void)head;
+	ft_printf("for now i'm doing nothing\n");
+	return (0);
+}
