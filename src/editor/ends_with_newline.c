1b487c205f5f85b18994050edaccbc924de68cfe
Marwa DAOUD <mdaoud@e2r7p9.42.fr>
2019-05-10 15:22:05 +0200

    Solved the shell input output from file

--- src/editor/ends_with_newline.c
+++ src/editor/ends_with_newline.c
@@ -6,32 +6,31 @@
 /*   By: mdaoud <mdaoud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 15:50:54 by mdaoud            #+#    #+#             */
-/*   Updated: 2019/05/09 16:45:47 by mdaoud           ###   ########.fr       */
+/*   Updated: 2019/05/10 15:17:21 by mdaoud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <shell21.h>
-#include <termios.h>
 
 static void		get_pos(int *row, int *col)
 {
 	char	buf[30];
 	int		i;
-	int		pow;
+	int		pow_var;
 	char	ch;
 
 	*row = 0; *col = 0;
 	ft_bzero(buf, 20);
-	write(1, "\033[6n", 4);
+	write(g_editor->tty_fd, "\033[6n", 4);
 	for( i = 0, ch = 0; ch != 'R'; i++ )
 	{
-		read(0, &ch, 1);
+		read(STDIN_FILENO, &ch, 1);
 		buf[i] = ch;
 	}
-	for (i -= 2, pow = 1; buf[i] != ';'; i--, pow *= 10)
-		*col = *col + ( buf[i] - '0' ) * pow;
-	for( i-- , pow = 1; buf[i] != '['; i--, pow *= 10)
-		*row = *row + ( buf[i] - '0' ) * pow;
+	for (i -= 2, pow_var = 1; buf[i] != ';'; i--, pow_var *= 10)
+		*col = *col + ( buf[i] - '0' ) * pow_var;
+	for( i-- , pow_var = 1; buf[i] != '['; i--, pow_var *= 10)
+		*row = *row + ( buf[i] - '0' ) * pow_var;
 }
 
 int				ends_with_newline(void)
