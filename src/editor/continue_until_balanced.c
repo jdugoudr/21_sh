8dddb51a043acb0ad3750a72e32e0db9690dec1e
Marwa DAOUD <mdaoud@e2r7p9.42.fr>
2019-05-10 17:41:23 +0200

    Solved the interlapping subshells

--- src/editor/continue_until_balanced.c
+++ src/editor/continue_until_balanced.c
@@ -6,7 +6,7 @@
 /*   By: mdaoud <mdaoud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 17:22:56 by mdaoud            #+#    #+#             */
-/*   Updated: 2019/05/10 16:40:24 by mdaoud           ###   ########.fr       */
+/*   Updated: 2019/05/10 17:33:51 by mdaoud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ void			continue_until_balanced(void)
 		prompt_set("dquote> ");
 	else if (q_flag == 4)
 		prompt_set("subsh> ");
-	else if ((q_flag | (SINGLE_Q | PARANTH)) != 0)
+	else if (q_flag == (SINGLE_Q | PARANTH))
 		prompt_set("subsh quote> ");
-	else if ((q_flag | (DOUBLE_Q | PARANTH)) != 0)
+	else if (q_flag == (DOUBLE_Q | PARANTH))
 		prompt_set("subsh dquote> ");
 	ft_dprintf(g_editor->tty_fd, "\n");
 	command_reset();
