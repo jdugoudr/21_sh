c5756a105660eee7cad770a0bfb83625e6135da3
Marwa DAOUD <mdaoud@e3r7p15.42.fr>
2019-05-20 16:10:58 +0200

    Changed the prompt message

--- src/editor/prompt_display.c
+++ src/editor/prompt_display.c
@@ -6,7 +6,7 @@
 /*   By: mdaoud <mdaoud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 17:08:26 by mdaoud            #+#    #+#             */
-/*   Updated: 2019/05/13 19:54:44 by mdaoud           ###   ########.fr       */
+/*   Updated: 2019/05/20 16:09:48 by mdaoud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void			prompt_display(void)
 {
-	// ft_dprintf(g_editor->tty_fd, "%s", g_editor->prompt);
+	ft_dprintf(g_editor->tty_fd, "%s", g_editor->prompt);
 
-	ft_dprintf(g_editor->tty_fd, "%s➜  %s%s%s ", GREEN, RED, g_editor->prompt, RES_COL);
+	// ft_dprintf(g_editor->tty_fd, "%s➜  %s%s%s ", GREEN, RED, g_editor->prompt, RES_COL);
 }
--- src/editor/prompt_reset.c
+++ src/editor/prompt_reset.c
@@ -17,6 +17,27 @@
 ** set the prompt to display the current working directory.
 */
 
+// void			prompt_reset(void)
+// {
+// 	char		cwd[PATH_MAX];
+// 	char		*dir_name;
+// 	int			root_dir;
+
+// 	ft_bzero(g_editor->prompt, PATH_MAX);
+// 	if (getcwd(g_editor->prompt, PATH_MAX) == NULL)
+// 	{
+// 		prompt_set("$>> ");
+// 		return ;
+// 	}
+// 	getcwd(cwd, PATH_MAX);
+// 	root_dir = !ft_strequ("/", cwd);
+// 	dir_name = ft_strrchr(cwd, '/');
+// 	if (dir_name == NULL)
+// 		dir_name = cwd;
+// 	ft_strcpy(g_editor->prompt, dir_name + root_dir);
+// 	g_editor->offset = ft_strlen(g_editor->prompt) + 4;
+// }
+
 void			prompt_reset(void)
 {
 	char		cwd[PATH_MAX];
@@ -34,6 +55,9 @@ void			prompt_reset(void)
 	dir_name = ft_strrchr(cwd, '/');
 	if (dir_name == NULL)
 		dir_name = cwd;
-	ft_strcpy(g_editor->prompt, dir_name + root_dir);
-	g_editor->offset = ft_strlen(g_editor->prompt) + 4;
+	ft_strcpy(g_editor->prompt, "-> ");
+	ft_strcat(g_editor->prompt, dir_name + root_dir);
+	ft_strcat(g_editor->prompt, ": ");
+	g_editor->offset = ft_strlen(g_editor->prompt);
+//	ft_dprintf(g_editor->tty_fd, "%s➜  %s%s%s ", GREEN, RED, g_editor->prompt, RES_COL);
 }
