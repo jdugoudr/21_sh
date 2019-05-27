5704eebad4952fef3190731c6087fb4311c08f4b
Marwa DAOUD <mdaoud@e2r12p17.42.fr>
2019-05-27 18:48:34 +0200

    clean

--- src/editor/remove_subshell_newline.c
+++ src/editor/remove_subshell_newline.c
@@ -6,7 +6,7 @@
 /*   By: mdaoud <mdaoud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 17:27:48 by mdaoud            #+#    #+#             */
-/*   Updated: 2019/05/27 18:42:22 by mdaoud           ###   ########.fr       */
+/*   Updated: 2019/05/27 18:46:10 by mdaoud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,33 +34,3 @@ void			remove_subshell_newline(void)
 	}
 	g_editor->cur_pos = pos;
 }
-
-// void			remove_subshell_newline(void)
-// {
-// 	size_t	i;
-// 	size_t	pos;
-
-// 	i = 0;
-// 	pos = g_editor->cur_pos;
-// 	while (g_editor->cmd[i])
-// 	{
-// 		if (g_editor->cmd[i] == '(' && g_editor->cmd[i + 1] == '\n')
-// 		{
-// 			// ft_printf("cur is on \'%c\'\n", g_editor->cmd[g_editor->cur_pos]);
-// 			// while (1);
-// 			g_editor->cur_pos = i + 2;
-// 			if (g_editor->cur_pos > g_editor->cmd_sze)
-// 				return ;
-// 			g_editor->cmd_sze--;
-// 			shift_string_left();
-// 		}
-// 		else if (g_editor->cmd[i] == '\n' && g_editor->cmd[i + 1] == ')')
-// 		{
-// 			g_editor->cmd_sze--;
-// 			g_editor->cur_pos = i + 1;
-// 			shift_string_left();
-// 		}
-// 		i++;
-// 	}
-// 	g_editor->cur_pos = pos;
-// }
