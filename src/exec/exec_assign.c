648a6c0fae577f6287db986c64cb813d9c9f8144
jdugoudr@student.42.fr <jdugoudr@e2r7p21.42.fr>
2019-05-28 20:42:01 +0200

    Change the print fd of assign message from stdin to stderr

--- src/exec/exec_assign.c
+++ src/exec/exec_assign.c
@@ -6,7 +6,7 @@
 /*   By: jdugoudr <jdugoudr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 11:07:59 by jdugoudr          #+#    #+#             */
-/*   Updated: 2019/05/28 10:46:51 by jdugoudr         ###   ########.fr       */
+/*   Updated: 2019/05/28 20:30:21 by jdugoudr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ int	exec_assign(t_ast *el, t_ast *head)
 {
 	(void)el;
 	(void)head;
-	ft_printf("21sh: This not implement yet sorry\n");
+	ft_dprintf(STDERR_FILENO, "21sh: This not implement yet sorry\n");
 	return (0);
 }
