b83c719601afd8818b06a8905b0310ced7421fa4
Marwa DAOUD <mdaoud@e2r7p23.42.fr>
2019-05-29 16:58:15 +0200

    norminette

--- src/parser/check_next/check_for_pipe.c
+++ src/parser/check_next/check_for_pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_for_pipe.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdugoudr <jdugoudr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mdaoud <mdaoud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/25 12:35:08 by jdugoudr          #+#    #+#             */
-/*   Updated: 2019/05/29 15:58:30 by jdugoudr         ###   ########.fr       */
+/*   Updated: 2019/05/29 16:58:02 by mdaoud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	check_for_pipe(t_ast *next, char **line)
 {
 	(void)line;
-
 	if ((next->type & AFTER_PIPE) == 0)
 		return (1);
 	return (0);
