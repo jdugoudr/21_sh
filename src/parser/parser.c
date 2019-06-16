7c4d667da3472fabd27bba3f4cf1decb2dbfb6e3
jdugoudr@student.42.fr <jdugoudr@e3r5p15.42.fr>
2019-06-16 16:03:37 +0200

    Subshell are also ok with new architecture

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: jdugoudr <jdugoudr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/24 15:09:14 by jdugoudr          #+#    #+#             */
-/*   Updated: 2019/06/15 15:26:37 by jdugoudr         ###   ########.fr       */
+/*   Updated: 2019/06/16 15:55:14 by jdugoudr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,24 +22,6 @@
 ** Each new token have to be check by the previous token
 ** to see if the new token is enable by grammar.
 */
-
-// static int	ambigous_redirect(t_ast *token)
-// {
-// 	while (token->next)
-// 	{
-// 		if (token->next->level_prior == level_4)
-// 		{
-// 			if (token->type != WORD_TOK
-// 				|| (token->prev && token->prev->type == WORD_TOK))
-// 			{
-// 				ft_dprintf(STDERR_FILENO, AMBI_REDIR);
-// 				return (1);
-// 			}
-// 		}
-// 		token = token->next;
-// 	}
-// 	return (0);
-// }
 
 static int	init_parser(char *line, t_ast **head, t_ast **root, int *r)
 {
@@ -68,14 +50,10 @@ int			parser(char *line)
 		ret = 1;
 	else if (token_head->next)
 	{
-		if (!(tmp = look_arg(token_head)) /*|| expansion_tok(token_head)
-				|| ambigous_redirect(token_head) || create_arg(token_head)*/
-				|| create_ast(&ast_root, token_head))
+		if (!(tmp = look_arg(token_head)) || create_ast(&ast_root, token_head))
 			ret = 1;
 		else
-		{
 			ret = run_ast(ast_root, token_head);
-		}
 	}
 	free(save_line);
 	del_ast(&token_head);
