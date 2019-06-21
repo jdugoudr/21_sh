c7e624ffdc211f4fe8d36caaa6af97a98ff2aeac
Marwa DAOUD <mdaoud@e2r2p2.42.fr>
2019-06-21 13:04:11 +0200

    Changed cd to no options

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: mdaoud <mdaoud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 19:12:43 by mdaoud            #+#    #+#             */
-/*   Updated: 2019/06/10 18:57:15 by mdaoud           ###   ########.fr       */
+/*   Updated: 2019/06/21 13:01:23 by mdaoud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,59 +49,6 @@ static void		repair_oldpwd(void)
 	add_env_var("OLDPWD", current_dir);
 }
 
-static char		*get_sy_link(char *str)
-{
-	char		*path;
-	struct stat	st;
-
-	path = NULL;
-	if (lstat(str, &st) < 0)
-	{
-		if ((access(str, F_OK) == -1))
-			ft_dprintf(STDERR_FILENO, "cd: no such file or directory: %s\n",
-				str);
-		else
-			ft_dprintf(STDERR_FILENO, INTERN_ERR);
-	}
-	else if ((st.st_mode & S_IFMT) == S_IFLNK)
-	{
-		if ((path = ft_strnew(256)) == NULL)
-			ft_dprintf(STDERR_FILENO, INTERN_ERR);
-		else
-			readlink(str, path, 256);
-	}
-	else
-		path = ft_strdup(str);
-	free(str);
-	return (path);
-}
-
-static int		loop_arg(char **cmd, char **path, int *print_dir, int *sy_link)
-{
-	int i;
-
-	i = 1;
-	if (ft_strequ("-P", cmd[1]))
-	{
-		*sy_link = 1;
-		i = 2;
-	}
-	else if (ft_strequ("-L", cmd[1]))
-		i = 2;
-	while (cmd[i])
-	{
-		if (ft_strequ("--", cmd[i]) == 0)
-			break ;
-		i++;
-	}
-	if ((*path = get_path(cmd[i], print_dir)) == NULL)
-	{
-		ft_dprintf(STDERR_FILENO, INTERN_ERR);
-		return (1);
-	}
-	return (0);
-}
-
 /*
 ** Checks if the "cd" command is well formatted.
 ** replaces special arguments such as "-" and "--" with their respective values
@@ -118,16 +65,12 @@ int				builtin_cd(char **cmd)
 	path = NULL;
 	print_dir = 0;
 	repair_oldpwd();
-	if (check_cmd_format(cmd, 0) == -1 && check_cmd_format(cmd, 1) == -1
-		&& check_cmd_format(cmd, 2) == -1 && check_cmd_format(cmd, 3) == -1)
+	if (check_cmd_format(cmd, 0) < 0 && check_cmd_format(cmd, 1) < 0)
 	{
-		write(STDERR_FILENO, "usage: cd [-L | -P] directory\n", 20);
+		write(STDERR_FILENO, "usage: cd directory\n", 20);
 		return (1);
 	}
-	if (loop_arg(cmd, &path, &print_dir, &sy_link))
-		return (1);
-	if (sy_link && (path = get_sy_link(path)) == NULL)
-		return (1);
+	path = get_path(cmd[1], &print_dir);
 	change_directory(path, print_dir);
 	free(path);
 	return (0);
