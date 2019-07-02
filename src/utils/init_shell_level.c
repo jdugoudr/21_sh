4bf4cc2f6dbd35c77ae0b784754fd4225bba31c4
daoudm <marwadaoud1993@gmail.com>
2019-07-03 01:52:17 +0200

    malloc protection, again..

--- src/utils/init_shell_level.c
+++ src/utils/init_shell_level.c
@@ -6,44 +6,71 @@
 /*   By: mdaoud <mdaoud@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 15:24:07 by mdaoud            #+#    #+#             */
-/*   Updated: 2019/06/20 15:50:53 by mdaoud           ###   ########.fr       */
+/*   Updated: 2019/07/03 01:51:20 by mdaoud           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shell21.h"
+#include "shell42.h"
 #include "libft.h"
 #include "sh_error.h"
 
 static int		set_env_value(size_t ind, char *key, char *val)
 {
-	free(g_shell->env[ind]);
-	if ((g_shell->env[ind] = ft_strjoin(key, "=", 0)) == NULL)
+	char	*tmp1;
+	char	*tmp2;
+	char	*backup;
+
+	(void)val;
+	backup = g_shell->env[ind];
+	if ((tmp1 = ft_strjoin(key, "=", 0)) == NULL)
 		return (1);
-	if ((g_shell->env[ind] = ft_strjoin(g_shell->env[ind], val, 1)) == NULL)
+	if ((tmp2 = ft_strjoin(tmp1, val, 0)) == NULL)
+	{
+		free(tmp1);
 		return (1);
+	}
+	g_shell->env[ind] = tmp2;
+	free(tmp1);
+	free(backup);
 	return (0);
 }
 
-void			init_shell_level(void)
+static void		raise_shell_level(int ind, char *tmp)
 {
 	int		shlvl;
-	int		ind;
-	char	*tmp;
-
-	ind = get_env_ind("SHLVL");
-	if (ind < 0)
-	{
-		add_env_var("SHLVL", "1");
-		return ;
-	}
-	tmp = get_env_value("SHLVL");
 	shlvl = ft_atoi(tmp) + 1;
 	free(tmp);
 	if ((tmp = ft_itoa(shlvl)) == NULL || set_env_value(ind, "SHLVL", tmp))
 	{
 		free(tmp);
-		ft_exit(INTERN_ERR, 1, 1, EXIT_FAILURE);
+		ft_dprintf(STDERR_FILENO, INTERN_ERR);
+		return ;
 	}
 	set_env_value(ind, "SHLVL", tmp);
 	free(tmp);
+}
+
+void			init_shell_level(void)
+{
+	int		ind;
+	char	*tmp;
+
+	ind = get_env_ind("SHLVL");
+	if (ind == -2)
+	{
+		if (add_env_var("SHLVL", "1"))
+			ft_dprintf(STDERR_FILENO, INTERN_ERR);
+		return ;
+	}
+	else if (ind == -1)
+	{
+		ft_dprintf(STDERR_FILENO, INTERN_ERR);
+		return ;
+	}
+	if ((tmp = get_env_value("SHLVL")) == NULL)
+	{
+		ft_dprintf(STDERR_FILENO, "malloc: could not get SHLVL variable\n");
+		return ;
+	}
+	raise_shell_level(ind, tmp);
 }
